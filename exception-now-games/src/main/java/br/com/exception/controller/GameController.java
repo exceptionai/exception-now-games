1e600c7cb4a0f3431dd9956b39181fd4fb6a5fdd
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2020-04-29 00:22:50 -0300

    Inicio do projeto

---
+++ exception-now-games/src/main/java/br/com/exception/controller/GameController.java
@@ -0,0 +1,16 @@
+package br.com.exception.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("game")
+public class GameController {
+	 
+	@GetMapping()
+	 public String home(Model model) {
+		return "index";
+	}
+}
