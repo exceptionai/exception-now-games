986b9983a859c89dff52f825f43c671c7e142d1b
tocalopes <49008354+tocalopes@users.noreply.github.com>
2020-06-09 20:36:06 -0300

    Update exception-now-games/src/main/resources/CREATE_GAMES.sql
    Co-authored-by: Vanessa Queiroz <40809283+vansolo@users.noreply.github.com>

--- exception-now-games/src/main/resources/CREATE_GAMES.sql
+++ exception-now-games/src/main/resources/CREATE_GAMES.sql
@@ -52,7 +52,7 @@ INSERT INTO TB_GAMES (name,description,genre,developer,publisher,price,image_url
     36.99,'https://tribogamer.com/jogos/imagens/banner/5689.jpg'
 );
 
-INSERT INTO GAMES (name,description,genre,developer,publiser,price,image_url) VALUES(
+INSERT INTO TB_GAMES (name,description,genre,developer,publisher,price,image_url) VALUES(
     'Dauntless',
     'Cuphead is a classic run and gun action game heavily focused on boss battles and inspired by cartoons of the 1930s. ',
     'Action, Indie', ' Studio MDHR Entertainment Inc.',
